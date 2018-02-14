c45cc22293cdd1f20ca0f0a43a69485e26ed34e7
udarange <udarange@gmail.com>
2018-02-14 15:08:38 +0530

    Change folder structure

--- index.js
+++ index.js
@@ -18,14 +18,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import reducer from './src/reducer';
-import { Container } from './src/containers';
+import reducer from './src/Reducer/reducer';
+import App from './src/Container'
 
 const store = createStore(reducer);
 
 render(
     <Provider store={store}>
-        <Container />
+        <App />
     </Provider>,
     document.getElementById('react')
 );
